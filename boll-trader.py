012c057ee5aabbb62a1db8e07f28c6afe09d0b7c
Jay.ha\MEDIUM <jay.ha@themedium.io>
2021-12-20 19:05:40 +0900

    boll-trader

--- boll-trader.py
+++ boll-trader.py
@@ -4,15 +4,40 @@ from arbi.arbi_in import *
 from arbi.arbi_out import *
 from main import *
 
-ex = Exchanges(testnet=True)
+import time
+from binance import ThreadedWebsocketManager
+from binance.enums import *
+
+f = open('.key.ini','r')
+keyJson = json.load(f)
+
+testnet = False
+if testnet:
+    api_key = keyJson['binance-testnet']['apiKey']
+    sec_key = keyJson['binance-testnet']['secKey']
+else:
+    api_key = keyJson['binance']['apiKey']
+    sec_key = keyJson['binance']['secKey']
+    
+
+def main():
+
+    symbol = 'BTCUSDT'
+
+    twm = ThreadedWebsocketManager(api_key=api_key, api_secret=sec_key)
+    # start is required to initialise its internal loop
+    twm.start()
+
+    def cb_kline(msg):
+        print(f'[kline]{msg}')
+    twm.start_kline_socket(callback=cb_kline, symbol=symbol, interval=KLINE_INTERVAL_3MINUTE)
+
+    def cb_depth(msg):
+        print(f'[depth]{msg}')
+    #twm.start_depth_socket(callback=cb_depth, symbol=symbol)
+    
+    twm.join()
 
 
-t_q = 1
-TEST = False
-UB_TEST = True
-BN_TEST = False
-
-
-#socke trading.
-
-#trading algorithm
+if __name__ == "__main__":
+   main()
--- remedy_in.py
+++ remedy_in.py
@@ -12,17 +12,22 @@ ex = Exchanges()
 
 asset = 'EOS'
 if True:
-    #t_q = 213.8 #not use
-    t_q_fee = 1575.7
-    inTh = 5.9
+    #t_q = 1586.8 #not use
+    t_q_fee = 1586.7
+    t_q = t_q_fee + 0.1 #eos tx fee
+    inTh = 6.0
 else:
     t_q_fee = 1333.0 #upbit spot, binance fut short q
     inTh = 3.6
 TEST = False
 
-asset_before = get_asset_total(ex, asset)
+log_open('log.txt')
+log(f"date:{datetime.now().strftime('%Y%m%d_%H%M%S')}")
+log(f"remedy: asset={asset}, t_q={t_q} t_q_fee={t_q_fee} IN_TH={inTh}")    
+#asset_before = get_asset_total(ex, asset)
 #arbi_in_withdraw_bn_to_ub(ex, asset, t_q, t_q_fee)
 arbi_in_ubSpotSell_bnFutBuy(ex, asset, t_q_fee, inTh, TEST)
 asset_after = get_asset_total(ex, asset)
-maxUSD = 5000
-print_arbi_stat(asset_before, asset_after, +inTh, maxUSD, ex.krwPerUsd)
+#maxUSD = 5000
+#print_arbi_stat(asset_before, asset_after, +inTh, maxUSD, ex.krwPerUsd)
+log_asset_total(ex, asset)
